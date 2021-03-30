740d9925ef468813cf352bda2e9ae2cc40ae5c67
unknown <nayaktitosh@gmail.com>
2021-03-30 09:26:16 +0530

    first commit

---
+++ age.js
@@ -0,0 +1,20 @@
+  
+								   //function code to be executed outerHTML
+								   
+								   
+								   function age(year)
+								   {
+									   console.log(2020-2010);
+								   }
+								   age(2010);
+								   
+								   // adding function
+								   
+								   function add(a)
+								   {
+									   console.log(a+5);
+									   
+									   
+								   }
+								   add(5);
+						
